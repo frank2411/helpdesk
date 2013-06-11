6f6c7ed45a16013969ccb2902377c6b8a02e3299
frank2411 <francesco.perna90@gmail.com>
2013-06-11 17:15:58 +0200

    modificato piccolo pezzo di code

--- partials/not-approved.php
+++ partials/not-approved.php
@@ -10,24 +10,24 @@
     $counter = 1;
     foreach ($toApprove as $post){ ?>
       <div class="post not_approved post_id_<?php echo $post->id?>">
-				<div class="postTitle">
-					<a href="/post/<?php echo $post->url ?>/">
-						<?php echo "<span class=\"counter\" >".$counter." .</span> ".$post->title; ?>
-					</a>
-				</div>
-				<div class="postText">
-					<?php echo $post->post; ?>
-				</div>
-				<?php /* 
-				<a class="approveButton radius" href="/post/approve/id/<?php echo $post->id ?>/">Approva Post</a>
-				<a class="deleteButton radius" href="/post/delete/id/<?php echo $post->id ?>/">Elimina Post</a>
-				*/
-				?>
-				<span class="approveButton radius <?php echo $post->id ?>">Approva Post</span>
-				<span class="deleteButton radius <?php echo $post->id ?>">Elimina Post</span>
+	<div class="postTitle">
+	  <a href="/post/<?php echo $post->url ?>/">
+	    <?php echo "<span class=\"counter\" >".$counter." .</span> ".$post->title; ?>
+	  </a>
+	</div>
+	<div class="postText">
+	  <?php echo $post->post; ?>
+	</div>
+	<?php /* 
+	<a class="approveButton radius" href="/post/approve/id/<?php echo $post->id ?>/">Approva Post</a>
+	<a class="deleteButton radius" href="/post/delete/id/<?php echo $post->id ?>/">Elimina Post</a>
+	*/
+      ?>
+	<span class="approveButton radius <?php echo $post->id ?>">Approva Post</span>
+	<span class="deleteButton radius <?php echo $post->id ?>">Elimina Post</span>
       </div>
     <?php  
-		$counter++;
+      $counter++;
     }
     
     //$singleStuff = $this->getCatBySlug("non-approvati");
